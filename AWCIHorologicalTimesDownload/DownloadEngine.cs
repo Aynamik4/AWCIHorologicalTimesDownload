06a2eb45632091b6dff85a44765d6148a7cdf293
Aynamik4 <violintailgut@gmail.com>
2024-09-04 18:10:46 +0200

    Add project files.

---
+++ AWCIHorologicalTimesDownload/DownloadEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWCIHorologicalTimesDownload;
+internal class DownloadEngine
+{
+    public async Task DownloadAndSave(string sourceFile, string destinationFolder, string destinationFileName)
+    {
+        Stream fileStream = await GetFileStream(sourceFile);
+
+        if (fileStream != Stream.Null)
+        {
+            await SaveStream(fileStream, destinationFolder, destinationFileName);
+        }
+    }
+
+    async Task<Stream> GetFileStream(string fileUrl)
+    {
+        HttpClient httpClient = new HttpClient();
+        try
+        {
+            Stream fileStream = await httpClient.GetStreamAsync(fileUrl);
+            return fileStream;
+        }
+        catch (Exception ex)
+        {
+            return Stream.Null;
+        }
+    }
+
+    static async Task SaveStream(Stream fileStream, string destinationFolder, string destinationFileName)
+    {
+        if (!Directory.Exists(destinationFolder))
+            Directory.CreateDirectory(destinationFolder);
+
+        string path = Path.Combine(destinationFolder, destinationFileName);
+
+        using (FileStream outputFileStream = new FileStream(path, FileMode.Create))
+        {
+            await fileStream.CopyToAsync(outputFileStream);
+        }
+    }
+}
---
+++ AWCIHorologicalTimesDownload/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AWCIHorologicalTimesDownload;
+
+enum Month
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var downloadEngine = new DownloadEngine();
+
+        for (int i = 2010; i < 2013; i++)
+        {
+            string fileName = "";
+
+            for (int m = (int)Month.January; m < 12; m++)
+            {
+                fileName = $"{(Month)m}{i}.pdf";
+                Console.WriteLine(fileName);
+
+                await downloadEngine.DownloadAndSave
+                    ($"https://www.awci.com/wp-content/uploads/ht/{fileName}",
+                    // https://www.awci.com/wp-content/uploads/ht/January2013.pdf
+                    "C:\\TMP5",
+                    fileName
+                    );
+            }
+        }
+    }
+}
