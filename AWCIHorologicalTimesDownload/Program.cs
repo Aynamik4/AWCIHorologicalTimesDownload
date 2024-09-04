9b00f74f69be7c0d42682023f0c4817169f738b8
Aynamik4 <violintailgut@gmail.com>
2024-09-04 19:07:08 +0200

    Added different output file name

--- AWCIHorologicalTimesDownload/Program.cs
+++ AWCIHorologicalTimesDownload/Program.cs
@@ -25,17 +25,19 @@ internal class Program
         for (int i = 2010; i < 2013; i++)
         {
             string fileName = "";
+            string outFileName = "";
 
             for (int m = (int)Month.January; m < 12; m++)
             {
                 fileName = $"{(Month)m}{i}.pdf";
-                Console.WriteLine(fileName);
+                outFileName = $"{i}-{(m + 1).ToString().PadLeft(2, '0')}-web.pdf";
+                Console.WriteLine(outFileName);
 
                 await downloadEngine.DownloadAndSave
                     ($"https://www.awci.com/wp-content/uploads/ht/{fileName}",
-                    // https://www.awci.com/wp-content/uploads/ht/January2013.pdf
+                    // https://www.awci.com/wp-content/uploads/ht/January2013.pdf // Download manually!
                     "C:\\TMP5",
-                    fileName
+                    outFileName
                     );
             }
         }
